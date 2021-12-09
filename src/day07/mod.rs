06585e00de022121dc3339bba6cb6b253d24dd3c
Christopher <leyssen@gmail.com>
2021-12-09 09:34:58 +0100

    make day7 more correct

--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -2,34 +2,35 @@ use std::str::FromStr;
 
 #[derive(Clone, Debug)]
 pub struct Crabs {
-    heights: Vec<u32>,
+    positions: Vec<u32>,
 }
 
 impl Crabs {
     fn median(&mut self) -> u32 {
-        self.heights.sort_unstable();
-        let len = self.heights.len();
+        self.positions.sort_unstable();
+        let len = self.positions.len();
+        // this check is not needed for this exercise but the method is called median so a median I shall provide
         if len % 2 == 0 {
-            (self.heights[len / 2 - 1] + self.heights[len / 2]) / 2
+            (self.positions[len / 2 - 1] + self.positions[len / 2]) / 2
         } else {
-            self.heights[len / 2]
+            self.positions[len / 2]
         }
     }
 
     #[allow(clippy::cast_precision_loss)]
     fn avg(&self) -> f32 {
-        self.heights.iter().sum::<u32>() as f32 / self.heights.len() as f32
+        self.positions.iter().sum::<u32>() as f32 / self.positions.len() as f32
     }
 
     fn total_align_distance(&self, align_height: u32) -> u32 {
-        self.heights
+        self.positions
             .iter()
             .map(|&height| delta(height, align_height))
             .sum()
     }
 
     fn align_cost(&self, align_height: u32) -> u32 {
-        self.heights
+        self.positions
             .iter()
             .map(|&height| cost(height, align_height))
             .sum()
@@ -54,25 +55,24 @@ impl FromStr for Crabs {
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
         let heights = input.split(',').map(|s| s.parse().unwrap()).collect();
-        Ok(Self { heights })
+        Ok(Self { positions: heights })
     }
 }
 
 pub fn part_1(mut crabs: Crabs) -> u32 {
     let median = crabs.median();
-    println!("median: {}", median);
     crabs.total_align_distance(median)
 }
 
 #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
 pub fn part_2(crabs: &Crabs) -> u32 {
     let avg = crabs.avg();
-    let min = avg.floor() as u32;
-    let max = avg.ceil() as u32;
-    println!("avg: {}, min: {}, max: {}", avg, min, max);
+    // the input file still works even when not adding or subtracting the 0.5 but this is mathematically
+    // the correct solution (deriving for the optimum tells us the solution is between (avg-0.5 and avg+0.5)
+    let min = (avg - 0.5).floor() as u32;
+    let max = (avg + 0.5).ceil() as u32;
     (min..=max)
         .map(|height| crabs.align_cost(height))
-        .inspect(|cost| println!("cost: {}", cost))
         .min()
         .unwrap()
 }
